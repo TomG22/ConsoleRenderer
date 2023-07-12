998f33a90e7f190e8bf1249c38c8847fabfb9f57
TomG22 <giallanzatom@gmail.com>
2023-07-12 10:18:39 -0700

    VertexObject complete

--- ConsoleRenderer/ConsoleRenderer/src/app.cpp
+++ ConsoleRenderer/ConsoleRenderer/src/app.cpp
@@ -7,17 +7,126 @@
 
 const float PI = 3.14159265f;
 
-const int REFRESH_TIME = 20; //ms
-const float TIME_RANGE = 1.0f; // unregulated units; 0 = still-image
-const float TIME_STEP = 0.02f;
-const float CALC_STEP = 0.05f; // lower value -> higher calculation precision
+const int REFRESH_TIME = 40; //ms
+const float TIME_RANGE = 2 * PI; // unregulated units; 0 = still-image
+const float TIME_STEP = .05;
+const float CALC_STEP = 0.01f; // lower value -> higher calculation precision
 
-const std::string FUNCTION = "ParamTorus";
+const std::string FUNCTION = "VertexObject";
 
-const int RADIUS = 30;
+const int RADIUS = 31;
 
 const int INNER_RADIUS = floor(RADIUS * .5);
 const int TUBE_RADIUS = floor(RADIUS * .25);
+
+float(*MatRx (float theta))[3] {
+	float Rx[3][3] = {
+		{1, 0, 0},
+		{0, cosf(theta), -sinf(theta)},
+		{0, sinf(theta), cosf(theta)}
+	};
+	return Rx;
+}
+
+float(*MatRy(float theta))[3] {
+	float Ry[3][3] = {
+		{cosf(theta), 0, sinf(theta)},
+		{0, 1, 0},
+		{-sinf(theta), 0, cosf(theta)}
+	};
+	return Ry;
+}
+
+float(*(MatRz)(float theta))[3] {
+	float Rz[3][3] = {
+		{cosf(theta), -sinf(theta), 0},
+		{sinf(theta), cosf(theta), 0},
+		{0, 0, 1}
+	};
+	return Rz;
+}
+
+float* MatTransform(float vector[3], float time) {
+	float transformed_vector[3];
+	for (int rowIdx = 0; rowIdx < 3; rowIdx++) {
+		float totalTransform = 0;
+		for (int colIdx = 0; colIdx < 3; colIdx++) {
+			float(*transformMatrix)[3];
+			transformMatrix = MatRx(time);
+			totalTransform += transformMatrix[rowIdx][colIdx] * vector[colIdx];
+		}
+		transformed_vector[rowIdx] = totalTransform;
+	}
+
+	float transformed_vector_2[3];
+	for (int rowIdx = 0; rowIdx < 3; rowIdx++) {
+		float totalTransform = 0;
+		for (int colIdx = 0; colIdx < 3; colIdx++) {
+			float(*transformMatrix)[3];
+			transformMatrix = MatRy(time);
+			totalTransform += transformMatrix[rowIdx][colIdx] * transformed_vector[colIdx];
+		}
+		transformed_vector_2[rowIdx] = totalTransform;
+	}
+	return transformed_vector_2;
+}
+
+std::string VertexObject(float time) {
+	std::string buffer[RADIUS * 2] = {};
+	for (int i = 0; i < RADIUS; i++) {
+		std::string buffer_row = "";
+		for (int j = 0; j < RADIUS * 2; j++) {
+			buffer_row.push_back(' ');
+		}
+		buffer[i] = buffer_row;
+	}
+
+	float vertices[8][3] = {
+		{10,10,10},
+		{10,10,-10},
+		{10,-10,10},
+		{10,-10,-10},
+		{-10,10,10},
+		{-10,10,-10},
+		{-10,-10,10},
+		{-10,-10,-10}
+	};
+
+	for (int i = 0; i < sizeof(vertices) / sizeof(vertices[0]); i++) {
+		float* vertexPtr = MatTransform(vertices[i], time);
+
+		int x = floor(vertexPtr[0] + RADIUS);
+		int y = floor((vertexPtr[1] + RADIUS) *.5);
+		int z = floor(vertexPtr[2] + RADIUS);
+
+		if (x >= 0 && y >= 0 && x < RADIUS * 2 && y < RADIUS) {
+			char map[] = "-=+^#%@";
+			int div = floor((z / float(RADIUS * 2 + 1)) * (sizeof(map) - 1));
+
+			if (buffer[y][x] != '*') {
+				for (int i = 0; i < sizeof(map); i++) {
+					if (buffer[y][x] == map[i]) {
+						if (div < i) {
+							div = i;
+						}
+					}
+				}
+			}
+			buffer[y][x] = map[div];
+		}
+		int half_radius = floor(RADIUS * .5);
+		buffer[half_radius][RADIUS] = '+';
+
+	}
+
+	std::string outputString;
+	for (int i = 0; i < RADIUS; i++) {
+		outputString.append(buffer[i]);
+		outputString.push_back('\n');
+	}
+
+	return outputString;
+}
 
 std::string ForCircle(float time) {
 	std::string buffer = "";
@@ -35,8 +144,6 @@ std::string ForCircle(float time) {
 
 std::string ParamCircle(float time) {
 	std::string buffer[RADIUS * 2] = {};
-	std::string string_buffer;
-
 	for (int i = 0; i < RADIUS; i++) {
 		std::string buffer_row = "";
 		for (int j = 0; j < RADIUS * 2; j++) {
@@ -46,29 +153,26 @@ std::string ParamCircle(float time) {
 	}
 
 	for (float i = 0; i <= 2 * PI; i += CALC_STEP) {
-		if (TIME_RANGE == 0) {
-			time = 1.0f;
-		}
-
-		int x = floor(time * RADIUS * cos(i) + RADIUS);
-		int y = floor(time * RADIUS * .5 * sin(i) + RADIUS * .5);
+		int x = floor(time * RADIUS * cosf(i) + RADIUS);
+		int y = floor(time * RADIUS * .5 * sinf(i) + RADIUS * .5);
 
 		if (x >= 0 && y >= 0 && x < RADIUS * 2 && y < RADIUS) {
 			buffer[y][x] = '#';
 		}
 	}
 
+	std::string outputString;
 	for (int i = 0; i < RADIUS; i++) {
-		string_buffer.append(buffer[i]);
-		string_buffer.push_back('\n');
+		outputString.append(buffer[i]);
+		outputString.push_back('\n');
 	}
 
-	return string_buffer;
+	return outputString;
 }
 
 std::string ParamTorus(float time) {
 	std::string buffer[RADIUS * 2] = {};
-	std::string string_buffer;
+	std::string outputString;
 
 	for (int i = 0; i < RADIUS; i++) {
 		std::string buffer_row = "";
@@ -84,14 +188,14 @@ std::string ParamTorus(float time) {
 				time = 1.0f;
 			}
 
-			int x = floor((INNER_RADIUS + TUBE_RADIUS * cos(v)) * cos(u) + RADIUS);
-			int y = floor(.5 * (INNER_RADIUS + TUBE_RADIUS * cos(v)) * sin(u) + RADIUS * .5);
-			int z = floor(TUBE_RADIUS * sin(v) + TUBE_RADIUS);
+			int x = floor((INNER_RADIUS + TUBE_RADIUS * cosf(v)) * cosf(u) + RADIUS);
+			int y = floor(.5 * (INNER_RADIUS + TUBE_RADIUS * cosf(v)) * sinf(u) + RADIUS * .5);
+			int z = floor(TUBE_RADIUS * sinf(v) + TUBE_RADIUS);
 
 			if (x >= 0 && y >= 0 && x < RADIUS * 2 && y < RADIUS) {
-				char map[]  = ".'!?xyWN";
+				char map[]  = "-=+*#%@";
 				int div = floor((z / float(TUBE_RADIUS*2 + 1)) * (sizeof(map)-1));
-				// std::cout << (z/(float(TUBE_RADIUS*2 + 1))) << "|";
+
 				if (buffer[y][x] != '*') {
 					for (int i = 0; i < sizeof(map); i++) {
 						if (buffer[y][x] == map[i]) {
@@ -107,30 +211,15 @@ std::string ParamTorus(float time) {
 	}
 
 	for (int i = 0; i < RADIUS; i++) {
-		string_buffer.append(buffer[i]);
-		string_buffer.push_back('\n');
+		outputString.append(buffer[i]);
+		outputString.push_back('\n');
 	}
 
-	return string_buffer;
+	return outputString;
 }
 
 void Clear() {
 	system("cls");
-}
-
-std::string DrawFunction(float time) {
-	if (FUNCTION == "ForCircle") {
-		return ForCircle(time);
-	}
-	else if (FUNCTION == "ParamCircle") {
-		return ParamCircle(time);
-	}
-	else if (FUNCTION == "ParamTorus") {
-		return ParamTorus(time);
-	}
-	else {
-		return "Unknown Function: \"" + FUNCTION + '"';
-	}
 }
 
 void DrawLoop() {
@@ -148,15 +237,14 @@ void DrawLoop() {
 				time = TIME_RANGE;
 			}
 			std::cout << "Rendering \"" << FUNCTION << "\" (" << REFRESH_TIME << "ms Video | Time: " << time << "):\n";
-			std::cout << DrawFunction(time);
+			std::cout << VertexObject(time);
 			std::this_thread::sleep_for(std::chrono::milliseconds(REFRESH_TIME));
 			Clear();
 			time += TIME_STEP * inc;
 		}
-	}
-	else {
+	} else {
 		std::cout << "Rendered \"" + FUNCTION << "\" (Image):\n";
-		std::cout << DrawFunction(0);
+		std::cout << VertexObject(0);
 	}
 }
 
